eb3f7ab3f70b3a211ff9628c13c54711f60a1701
zone <43332748+plateau11@users.noreply.github.com>
2025-07-31 16:22:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,13 @@ from clipboard_data import set_clipboard, get_clipboard
 
 app = Flask(__name__)
 
+gif_urls = [
+    "https://raw.githubusercontent.com/plateau11/test99/main/1f5832186ffd64058efad2a3a810d006.gif",
+    "https://raw.githubusercontent.com/plateau11/test99/main/0e1f8367d81e6f161d198a0b5011a62f.gif",
+    "https://raw.githubusercontent.com/plateau11/test99/main/92cdfc9bdebc53a747331999b6933734.gif",
+    "https://raw.githubusercontent.com/plateau11/test99/main/tumblr_nnozvnkRtE1uuzayro1_1280.gif"
+]
+
 @app.route('/')
 def home():
     return render_template("index.html")  # Optional UI
@@ -20,11 +27,10 @@ def set_clipboard_route():
     set_clipboard(text)
     return jsonify({"message": "Clipboard updated", "clipboard": text})
 
-# ✅ New endpoint to return a GIF URL
 @app.route('/getGifUrl', methods=['GET'])
 def get_gif_url():
-    gif_url = "https://raw.githubusercontent.com/plateau11/test99/main/1f5832186ffd64058efad2a3a810d006.gif"
-    return jsonify({"gif_url": gif_url})
+    selected_url = random.choice(gif_urls)
+    return jsonify({"gif_url": selected_url})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=6000, debug=True)
